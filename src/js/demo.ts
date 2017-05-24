6310a0d4a352305db167b8c55d94902082cc0144
Nick Rigby <nick.rigby@gmail.com>
2017-05-23 23:18:39 -0400

    Add webpack and dev server

---
+++ src/js/demo.ts
@@ -0,0 +1,11 @@
+import "../scss/_variables.scss";
+import "../scss/_index.scss";
+
+import { SilcCore } from "silc-core";
+import { SilcNav } from "./index";
+
+new SilcCore();
+
+[].forEach.call(document.querySelectorAll('.silc-nav'), (el) => {
+    new SilcNav(el);
+});
